5f5e7268ffd1ca89859570ed4fc8c704e22d07ab
luicode <jefflui7@gmail.com>
2026-06-02 11:00:51 -0400

    update

--- app/mailers/email_signup_mailer.rb
+++ app/mailers/email_signup_mailer.rb
@@ -8,6 +8,7 @@ class EmailSignupMailer < ApplicationMailer
 
   def admin_notification(email_signup)
     @email_signup = email_signup
+    @submitted_at = email_signup.created_at.in_time_zone("America/Antigua").strftime("%B %-d, %Y at %-I:%M %p AST")
 
     mail(
       to: ADMIN_RECIPIENTS,
--- test/mailers/email_signup_mailer_test.rb
+++ test/mailers/email_signup_mailer_test.rb
@@ -2,12 +2,13 @@ require "test_helper"
 
 class EmailSignupMailerTest < ActionMailer::TestCase
   test "admin notification" do
-    signup = EmailSignup.new(email: "guest@example.com", created_at: Time.current)
+    signup = EmailSignup.new(email: "guest@example.com", created_at: Time.utc(2026, 6, 2, 14, 30))
     mail = EmailSignupMailer.admin_notification(signup)
 
     assert_equal ["jason@boardwalkcasino.ag", "annettelui@gmail.com", "jefflui7@gmail.com", "claireycyang@gmail.com"], mail.to
     assert_equal ["boardwalkcasinoantigua@gmail.com"], mail.from
     assert_equal "New Boardwalk Prime email signup", mail.subject
     assert_match "guest@example.com", mail.body.encoded
+    assert_match "June 2, 2026 at 10:30 AM AST", mail.body.encoded
   end
 end
